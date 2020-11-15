f37d8e396c4adae8d2879a78b7b8a5b9d93a9ea2
Artur Tamborski <tamborskiartur@gmail.com>
2020-11-15 01:29:27 +0100

    components: create new dir, move code there

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Logo from '../Logo/Logo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../styles/Logo.css';
+
+function Logo() {
+  return (
+    <div className="Logo">
+      asdsdasdadsads
+    </div>
+  );
+}
+
+export default Logo;
